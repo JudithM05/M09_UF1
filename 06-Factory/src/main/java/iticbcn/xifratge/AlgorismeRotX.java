0e2a729e60955b47d72b5b264e9c314f7c754562
JudithM05 <2023_judith.mera@iticbcn.cat>
2024-11-06 19:37:17 +0100

    Ho tinc fet des de fa uns dies, però acabo d'arreglar un petit error del que no m'he donat compte

--- 06-Factory/src/main/java/iticbcn/xifratge/AlgorismeRotX.java
+++ 06-Factory/src/main/java/iticbcn/xifratge/AlgorismeRotX.java
@@ -3,6 +3,6 @@ package iticbcn.xifratge;
 public class AlgorismeRotX extends AlgorismeFactory {
     @Override
     public Xifrador creaXifrador() {
-        return new XifradorPoliAlfabetic();
+        return new XifradorRotX();
     }
 }
